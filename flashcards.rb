5775fa989b1771134723374ec95251d40a10ef82
Pivotal <pivotal@academy.la.pivotallabs.com>
2017-06-18 16:47:06 -0700

    initial commit

---
+++ flashcards.rb
@@ -0,0 +1,54 @@
+require 'sinatra'
+require "sqlite3"
+
+
+db = SQLite3::Database.new "test.db"
+
+# Create a table
+rows = db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS cards (
+    front text,
+    back text
+  );
+SQL
+
+
+# I think this returns an Enumrable
+# which means instead of a block you can probably just call #to_a
+# so get rid of the do...end and relace it with .to_a
+puts "this is line #{__LINE__} happening"
+
+
+# GET :index
+get '/' do
+  #puts "this is line #{__LINE__} happening"
+  flashcards = db.execute("select * from cards").to_a
+
+  # get all the cards out of the db
+  # put them into an array
+  # render the index.html.erb with those cards passed in
+  erb :index, locals: {flashcards: flashcards}
+end
+
+
+get '/new' do
+  erb :new
+end
+
+post '/create' do
+  db.execute("INSERT INTO cards (front, back) VALUES (?, ?)", params["flashcard_front"],params["flashcard_back"])
+
+  redirect '/'
+end
+
+get '/study' do
+  offset = (params[:offset] || 0).to_i
+  flipped = params[:flipped] == "true"
+
+  if flipped
+    card = db.execute("select front from cards limit 1 offset ?", offset).first.first
+  else
+    card = db.execute("select back from cards limit 1 offset ?", offset).first.first
+  end
+  erb :study, locals: {card: card, flipped: flipped, offset: offset}
+end
